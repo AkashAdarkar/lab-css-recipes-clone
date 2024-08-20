862ff34e4e7a46bbbd00f998b46a37af49d6e63c
AkashAdarkar <aregret01@gmail.com>
2024-08-21 01:02:53 +0530

    newspaper complete

---
+++ news-paper/index.js
@@ -0,0 +1,8 @@
+const date = new Date()
+
+document.getElementById('date').innerHTML= date.toLocaleDateString();
+// const paragraphs = document.querySelectorAll('p');
+
+// paragraphs.forEach(paragraph => {
+//     paragraph.innerHTML += '<br>';
+// });
